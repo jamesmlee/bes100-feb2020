4b28b9699db98c2eb6f3919153d6afb4f2652315
James Lee <jamesminlee@gmail.com>
2020-02-25 20:44:07 +0000

    day 2

--- LibraryApi/Controllers/StatusController.cs
+++ LibraryApi/Controllers/StatusController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LibraryApi.Services;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,73 @@ namespace LibraryApi.Controllers
             };
             return Ok(response);
         }
+
+        // Resource Archetypes
+        // 1. Collection (usually plural, a set of things) /employees
+        // 2. Document (singular ... a single thingy)
+        // 3. Store (server-maintained cache of client data
+        // 4. Controller
+
+        // Getting information INTO the API
+        // 1. URL
+        //    a. Just plain ole' routing.
+        //    b. Embed some variables in the route itself. e.g. GET /employees/657/salary
+        
+        // employeeId: int ... route constraint. without it, GET /employees/bob/salary will get a response of 0, without it 404 status code
+        // employeeId:int:min(1) ... preferred over if(employeeId < 1) { return NotFound() };
+        [HttpGet("employees/{employeeId:int}/salary")]
+        public ActionResult GetEmployeeSalary(int employeeId)
+        {
+            //if (employeeId < 1)
+            //{
+            //    return NotFound();
+            //}
+            return Ok($"Employee {employeeId} has a salary of $65,000");
+        }
+
+        //    c. Add some data to the query string at the end of the URL e.g. GET /shoes?color=blue
+
+        // can't use constraints with query string parameters
+        [HttpGet("shoes")]
+        public ActionResult GetSomeShoes([FromQuery] string color = "All")
+        {
+            return Ok($"Getting you shoes of color {color}");
+        }
+
+        [HttpGet("whoami")]
+        public ActionResult WhoAmI([FromHeader(Name="User-Agent")] string userAgent)
+        {
+            return Ok($"You are using {userAgent}");
+        }
+
+        [HttpGet("employees")]
+        public ActionResult GetAllEmployees()
+        {
+            return Ok("All the employees...");
+        }
+
+        [HttpPost("employees")]
+        public ActionResult AddAnEmployee([FromBody] NewEmployee employee, [FromServices] IGenerateEmployeeIds idGenerator)
+        {
+            // new keyword means tightly coupled to a service
+            //var idGenerator = new EmployeeIdGenerator();
+            // instead, add IGenerateEmployeeIds to Startup.cs in Configure Services
+            var id = idGenerator.GetNewEmployeeId();
+            return Ok($"Hiring {employee.Name} starting at {employee.StartingSalary.ToString("c")} with id of {id.ToString()}");
+        }
+        /* Body
+         {
+	        "name": "Bob Smith",
+	        "startingSalary": 52000
+         } 
+        */
+    }
+
+    // after copying JSON, go to Edit --> Paste Special --> Paste JSON As Classes
+    public class NewEmployee
+    {
+        public string Name { get; set; }
+        public decimal StartingSalary { get; set; }
     }
 
     public class StatusResponse
@@ -25,4 +93,6 @@ namespace LibraryApi.Controllers
         public string Status { get; set; }
         public DateTime CreatedAt { get; set; }
     }
+
+
 }
--- LibraryApi/Startup.cs
+++ LibraryApi/Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibraryApi.Domain;
+using LibraryApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +27,18 @@ namespace LibraryApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // AddTransient, AddScoped, AddSingleton
+            services.AddTransient<IGenerateEmployeeIds, EmployeeIdGenerator>();
             services.AddControllers();
+            services.AddDbContext<LibraryDataContext>(options =>
+            // do NOT hardcode connection strings like this ... just for example
+                //options.UseSqlServer(@"server=.\sqlexpress;database=library;integrated security=true")
+                options.UseSqlServer(Configuration.GetConnectionString("LibraryDatabase"))
+            // move to appSettings.json, or if running in Docker, as an environment variable where 
+            // name: ConnectionStrings__LibraryDatabase ... value: server=.\sqlexpress;database=library;integrated security=true
+            );
+            // in Package Manager Console: add-migration initial
+            // in Package Manager Console: update-database
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
