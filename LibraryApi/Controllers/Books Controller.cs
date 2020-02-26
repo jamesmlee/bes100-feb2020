a877a5b7ce86520eb83c835b79eb8a34f411983d
James Lee <jamesminlee@gmail.com>
2020-02-26 14:04:53 +0000

    day 3 morning

---
+++ LibraryApi/Controllers/Books Controller.cs
@@ -0,0 +1,39 @@
+﻿using LibraryApi.Domain;
+using LibraryApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class Books_Controller : Controller
+    {
+        LibraryDataContext Context;
+
+        public Books_Controller(LibraryDataContext context)
+        {
+            Context = context;
+        }
+
+        [HttpGet("books")]
+        public ActionResult GetAllBooks([FromQuery] string genre = "all")
+        {
+            // this leaks too much to the client
+            //var response = Context.Books.ToList();
+            var response = new GetBooksResponse();
+            var data = Context.Books
+                .Where(b => b.InInventory);
+
+            if (genre != "all")
+            {
+                data = data.Where(b => b.Genre == genre);
+            }
+
+            response.Data = data.Select(b => new BookSummaryItem { Id = b.Id, Title = b.Title, Author = b.Author })
+                .ToList();
+            return Ok(response);
+        }
+    }
+}
---
+++ LibraryApi/Services/EmployeeIdGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Services
+{
+    // click class name --> screwdriver --> Extract Interface
+    public class EmployeeIdGenerator : IGenerateEmployeeIds
+    {
+        public Guid GetNewEmployeeId()
+        {
+            return Guid.NewGuid();
+        }
+    }
+}
